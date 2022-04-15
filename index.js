4887e3abac09f7e8dc6f0030ee6907f1d6ea2cb2
Maxim87 <maksboks87@mail.ru>
2022-04-15 16:45:09 +0700

    Add buttons, function start, function againOptions

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const bot = new TelegramApi(token, {polling: true})
 
 const chats = {}
 
+const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
+            [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
+            [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
+            [{text: '0', callback_data: '0'}]
+        ]
+    })
+}
 
+const againOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Play again', callback_data: '/again'}]
+        ]
+    })
+}
 
 const start = () => {
 
@@ -31,11 +48,22 @@ const start = () => {
             await bot.sendMessage(chatId, 'Now I say you number from 0 to 9. You need guess it')
             const randomNumber = Math.floor(Math.random() * 10)
             chats[chatId] = randomNumber;
-            return bot.sendMessage(chatId, 'Guess');
+            return bot.sendMessage(chatId, 'Guess', gameOptions);
         }
         return bot.sendMessage(chatId, 'I dont understand you')
 
     })
+
+    bot.on('callback_query', msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id
+        if (data === chats[chatId]) {
+          return bot.sendMessage(chatId, `Congratulation!!!It's ${chats[chatId]}`, againOptions)
+        } else {
+          return bot.sendMessage(chatId, `You lose :(, it was ${chats[chatId]}`, againOptions)
+        }
+    })
+
 }
 
 start()
